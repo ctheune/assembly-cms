4e510956221962e14393bc388b25a1d269a5610d
Christian Theune <ct@flyingcircus.io>
2009-08-02 16:00:43 +0200

    Implement a very simple retail variation selector.

--- src/asm/cms/retail.py
+++ src/asm/cms/retail.py
@@ -12,3 +12,15 @@ class Layout(megrok.pagelet.Layout):
     grok.layer(asm.cms.interfaces.IRetailSkin)
 
     megrok.pagelet.template('templates/retail.pt')
+
+
+class Index(grok.View):
+
+    grok.context(asm.cms.interfaces.ILocation)
+    grok.layer(asm.cms.interfaces.IRetailSkin)
+
+    def render(self):
+        session = zope.session.interfaces.ISession(self.request)
+        variation = self.context.getVariation(session['asm.cms']['variation'])
+        return zope.component.getMultiAdapter((variation, self.request),
+                                              name='index')()

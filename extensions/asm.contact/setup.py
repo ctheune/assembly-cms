f4dc1e20034355b174c0986f5b38705b6cbaff5a
Atte Timonen <atte.timonen@iki.fi>
2009-08-04 12:21:33 +0300

    Extract asm.contact to its own package.

---
+++ extensions/asm.contact/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(name='asm.contact',
+      version='0.1',
+      description="Contact form micro-application for Assembly CMS",
+      author="Webcrew",
+      author_email="web@assembly.org",
+      url="",
+      license="proprietary",
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'grok',
+                        'asm.cms',
+                        ],
+      )

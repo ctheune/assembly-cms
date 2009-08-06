c2f638001d2dbf38b7d19432b9428e35584e082e
Christian Theune <ct@gocept.com>
2009-08-06 16:35:00 +0300

    Snapshot: start integrating a server-side link browser

--- src/asm/cms/static/tinymce_widget.js
+++ src/asm/cms/static/tinymce_widget.js
@@ -1,13 +1,25 @@
 tinyMCE.init({
-    mode: "specific_textareas",
-    editor_selector: "mceEditor",
+    mode: 'specific_textareas',
+    editor_selector: 'mceEditor',
 
-    theme: "advanced",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
+    plugins : 'inlinepopups',
 
-    plugins : "inlinepopups",
-    dialog_type : "modal",
+    theme: 'advanced',
+    theme_advanced_toolbar_location : 'top',
+    theme_advanced_toolbar_align : 'left',
+    dialog_type : 'modal',
+
+    file_browser_callback: 'asmcmsFileBrowser',
 
     gecko_spellcheck : true
 });
+
+
+function asmcmsFileBrowser(field_name, url, type, win) {
+
+    tinyMCE.activeEditor.windowManager.open({
+        url: window.location + '/@@tinymce-linkbrowser',
+        inline: "yes",
+    });
+    return false;
+}

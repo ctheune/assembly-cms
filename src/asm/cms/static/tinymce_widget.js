490586524a598d9e5ebdd5699a618ad812cd5e2f
Christian Theune <ct@gocept.com>
2009-08-06 15:04:09 +0300

    Replace FCK with TinyMCE

---
+++ src/asm/cms/static/tinymce_widget.js
@@ -0,0 +1,9 @@
+tinyMCE.init({
+    mode: "specific_textareas",
+    editor_selector: "mceEditor",
+
+    theme: "advanced",
+    theme_advanced_toolbar_location : "top",
+
+    gecko_spellcheck : true
+});

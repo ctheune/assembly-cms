e707205e3c130c5f6437dfdd5ba622bfa7f69d93
Christian Theune <ct@gocept.com>
2009-08-06 15:16:00 +0300

    Left-align WYSIWYG toolbar, make popups stay inline and modal.

--- src/asm/cms/static/tinymce_widget.js
+++ src/asm/cms/static/tinymce_widget.js
@@ -4,6 +4,10 @@ tinyMCE.init({
 
     theme: "advanced",
     theme_advanced_toolbar_location : "top",
+    theme_advanced_toolbar_align : "left",
+
+    plugins : "inlinepopups",
+    dialog_type : "modal",
 
     gecko_spellcheck : true
 });

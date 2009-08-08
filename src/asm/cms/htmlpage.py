85999a687292e8cd396e2d27920a7674bd4787a5
Tarmo Toikkanen <tarmo.toikkanen@iki.fi>
2009-08-08 04:35:51 +0300

    Added new metadata fields. Closes #8.

--- src/asm/cms/htmlpage.py
+++ src/asm/cms/htmlpage.py
@@ -15,9 +15,31 @@ class HTMLPage(asm.cms.edition.Edition):
     zope.interface.implements(asm.cms.interfaces.IHTMLPage)
     zope.interface.classProvides(asm.cms.interfaces.IEditionFactory)
 
-    content = u''
+    _content = u''
+    def content():
+        def fget(self):
+            return self._content
+
+        def fset(self,value):
+            self._content = value
+            self._title=scrape_title(value)
+        return locals()
+    content = property(**content())
+
+    _title = u''
+    def title():
+        doc="Title that cannot be mutated"
+        def fget(self):
+            return self._title
+
+        def fset(self,value):
+            # Don't accept new titles this way!
+            pass #XXX or raise?
+        return locals()
+    title = property(**title())
 
     def copyFrom(self, other):
+        # NB. This will magically update self.title as well.
         self.content = other.content
 
 
@@ -29,5 +51,8 @@ class Edit(asm.cms.form.EditForm):
 
     grok.layer(asm.cms.interfaces.ICMSSkin)
 
-    form_fields = grok.AutoFields(asm.cms.interfaces.IHTMLPage)
+    form_fields = reduce(
+        lambda x,y: x.omit(y),
+        ['parameters','title','tags','date_created','date_modified'],
+        grok.AutoFields(HTMLPage))
     form_fields['content'].custom_widget = asm.cms.tinymce.TinyMCEWidget

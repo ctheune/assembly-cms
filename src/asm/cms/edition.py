2c4a81c08c24313b143ce1b2e81e459b7eb7f740
Christian Theune <ct@gocept.com>
2009-08-08 19:37:32 +0300

    Start filling the link browser template with live. I currently can't include the popup because of a missing resource inclusion of hurry.tinymce.

--- src/asm/cms/edition.py
+++ src/asm/cms/edition.py
@@ -3,7 +3,10 @@
 
 import BTrees.OOBTree
 import asm.cms.interfaces
+import datetime
 import grok
+import megrok.pagelet
+import pytz
 import re
 import zope.interface
 
@@ -12,9 +15,16 @@ class Edition(grok.Model):
 
     zope.interface.implements(asm.cms.interfaces.IEdition)
 
+    created = None
+    modified = None
+    tags = None
+    title = u''
+
     def __init__(self):
         super(Edition, self).__init__()
         self.parameters = BTrees.OOBTree.OOTreeSet()
+        self.date_created = datetime.datetime.now()
+        self.date_modified = self.date_created
 
     def editions(self):
         return self.__parent__.editions
@@ -22,6 +32,30 @@ class Edition(grok.Model):
     @property
     def page(self):
         return self.__parent__
+
+    def copyFrom(self, other):
+        self.created = other.created
+        self.modified = other.modified
+        self.tags = other.tags
+        self.title = other.title
+        zope.event.notify(grok.ObjectModifiedEvent(self))
+
+
+grok.context(Edition)
+
+
+class NullEdition(Edition):
+    pass
+
+
+class NullIndex(megrok.pagelet.Pagelet):
+
+    grok.layer(asm.cms.ICMSSkin)
+    grok.name('index')
+    grok.context(NullEdition)
+
+    def render(self):
+        return 'No edition available.'
 
 
 class EditionParameters(object):
@@ -36,6 +70,8 @@ class EditionParameters(object):
         self.parameters = set(initial)
 
     def __eq__(self, other):
+        if not other:
+            return False
         return self.parameters == other.parameters
 
     def __iter__(self):
@@ -92,9 +128,9 @@ class Actions(grok.Viewlet):
     grok.context(Edition)
 
 
-# XXX The following viewlet setup is a bit annoying: we register a viewlet for
-# displaying all editions when looking at a page and when looking at a
-# specific edition. The code is basically the same each time (we actually
+# Issue #59: The following viewlet setup is a bit annoying: we register a
+# viewlet for displaying all editions when looking at a page and when looking
+# at a specific edition. The code is basically the same each time (we actually
 # re-use the template), but the amount of registration necessary is just bad.
 
 class Editions(grok.ViewletManager):
@@ -108,7 +144,39 @@ class PageEditions(grok.Viewlet):
     grok.template('editions')
 
 
-class TinyMCELinkBrowsers(grok.View):
+@grok.subscribe(asm.cms.interfaces.IEdition, grok.IObjectModifiedEvent)
+def annotate_modification_date(obj, event):
+    obj.modified = datetime.datetime.now(pytz.UTC)
 
-    grok.name('tinymce-linkbrowser')
-    grok.template('tinymce-linkbrowser')
+
+@grok.subscribe(Edition, grok.ObjectAddedEvent)
+def annotate_creation_date(obj, event):
+    obj.created = datetime.datetime.now(pytz.UTC)
+
+
+def select_edition(page, request):
+    editions = dict((x, 0) for x in page.editions)
+    for selector in zope.component.subscribers(
+        (page, request), asm.cms.interfaces.IEditionSelector):
+        # Clean out all editions which are neither preferred nor accepted
+        # by the current selector
+        selected = set()
+        selected.update(selector.preferred)
+        selected.update(selector.acceptable)
+        for edition in list(editions.keys()):
+            if edition not in selected:
+                del editions[edition]
+
+        for edition in selector.preferred:
+            if edition in editions:
+                editions[edition] += 1
+
+    if not editions:
+        null = NullEdition()
+        null.__parent__ = page
+        null.__name__ = u''
+        return null
+
+    editions = editions.items()
+    editions.sort(key=lambda x:x[1], reverse=True)
+    return editions[0][0]
--- src/asm/cms/static/tinymce_widget.js
+++ src/asm/cms/static/tinymce_widget.js
@@ -2,14 +2,25 @@ tinyMCE.init({
     mode: 'specific_textareas',
     editor_selector: 'mceEditor',
 
-    plugins : 'inlinepopups',
+    plugins : 'inlinepopups,fullscreen,table',
 
     theme: 'advanced',
     theme_advanced_toolbar_location : 'top',
     theme_advanced_toolbar_align : 'left',
+    theme_advanced_buttons1: "formatselect,|,bold,italic,strikethrough,|,justifyleft,justifycenter,justifyright,|,bullist,numlist,table,|,link,unlink,image,|,code,fullscreen",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: "",
+
+    theme_advanced_blockformats: "p,h2,h3,blockquote,code",
+
     dialog_type : 'modal',
 
     file_browser_callback: 'asmcmsFileBrowser',
+
+    document_base_url: document.baseURI,
+
+    width: 600,
+    height: 800,
 
     gecko_spellcheck : true
 });
@@ -18,8 +29,9 @@ tinyMCE.init({
 function asmcmsFileBrowser(field_name, url, type, win) {
 
     tinyMCE.activeEditor.windowManager.open({
-        url: window.location + '/@@tinymce-linkbrowser',
+        url: document.baseURI + '/@@tinymce-linkbrowser',
         inline: "yes",
+
     });
     return false;
 }

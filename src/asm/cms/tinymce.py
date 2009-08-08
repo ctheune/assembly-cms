16be9057ac609558cb4a8f843af100418c11c4ab
Christian Theune <ct@flyingcircus.io>
2009-08-08 19:37:32 +0300

    Start filling the link browser template with live. I currently can't include the popup because of a missing resource inclusion of hurry.tinymce.

--- src/asm/cms/tinymce.py
+++ src/asm/cms/tinymce.py
@@ -1,18 +1,32 @@
 # Copyright (c) 2009 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import grok
 import zope.app.form.browser.textwidgets
 import hurry.tinymce.tinymce
 import hurry.resource
+import asm.cms.interfaces
 
 widget_lib = hurry.resource.Library('asm.cms')
 widget = hurry.resource.ResourceInclusion(widget_lib, 'tinymce_widget.js',
-                                 depends=[hurry.tinymce.tinymce])
+                                 depends=[hurry.tinymce.tinymce],
+                                 bottom=True)
 
 
 class TinyMCEWidget(zope.app.form.browser.textwidgets.TextAreaWidget):
 
     def __call__(self):
         widget.need()
+        hurry.resource.bottom(force=True)
         self.cssClass += ' mceEditor'
         return super(TinyMCEWidget, self).__call__()
+
+
+class TinyMCELinkBrowser(grok.View):
+
+    grok.context(asm.cms.interfaces.IPage)
+    grok.name('tinymce-linkbrowser')
+    grok.template('linkbrowser')
+
+    def update(self):
+        hurry.tinymce.tinymce.need()

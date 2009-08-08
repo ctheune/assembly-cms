6f6277c99d3b84cdf0b007f1fbd964f3a4748663
Christian Theune <ct@flyingcircus.io>
2009-08-08 16:49:11 +0300

    Provide testing infrastructure for extension modules.

--- test-all.sh
+++ test-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bin/test-core && bin/test-workflow && bin/test-translation && bin/test-schedule && bin/test-contact
+bin/test-core && bin/test-workflow && bin/test-translation && bin/test-schedule && bin/test-contact && bin/test-summer09
 
 echo "Success $?"

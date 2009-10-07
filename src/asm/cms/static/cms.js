595f7a0fffead04fb394fa0fd1662a8102367285
Christian Theune <ct@gocept.com>
2009-10-07 18:01:07 +0200

    Allow toggling navigation by pressing [ESC].

--- src/asm/cms/static/cms.js
+++ src/asm/cms/static/cms.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
+
+  // Showing/hiding navigation screen
   $(document).keydown(function(e) {
         if (e.which == 27) {
-            hide_navigation();
+            toggle_navigation();
         }});
 
-  $("#menu-navigation-handle").click(function () {
-    $("#navigation-wrapper").show();
-    $("body").css('overflow', 'hidden');
-  });
+  $("#menu-navigation-handle").click(show_navigation);
   $("#navigation-wrapper .menu-head").click(hide_navigation);
+
+  // Folder sorting
   $("#sortable").sortable({update: update_order});
 });
 
@@ -18,7 +19,16 @@ function update_order(event, ui) {
     //alert($("#subpages").elements);
 }
 
-function hide_navigation(){
+function hide_navigation() {
     $("#navigation-wrapper").hide();
     $("body").css('overflow', 'scroll');
+    toggle_navigation = show_navigation;
 }
+
+function show_navigation() {
+    $("#navigation-wrapper").show();
+    $("body").css('overflow', 'hidden');
+    toggle_navigation = hide_navigation;
+}
+
+toggle_navigation = show_navigation;

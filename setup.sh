de427bd004297f2cadc001fcb04d6440c004ffd0
Jussi Judin <jjudin+bitbucket@iki.fi>
2009-11-01 17:06:30 +0200

    Removing unneeded setuptools installation.

--- setup.sh
+++ setup.sh
@@ -10,14 +10,12 @@ done
 cp buildout.cfg.example buildout.cfg || exit 1
 
 PYTHON_VERSION="2.5.4"
-SETUPTOOLS_VERSION="0.6c9"
 
 APP_ROOT="`pwd`"
 PACKAGES_ROOT="$APP_ROOT"/packages
 PYTHON_ROOT="$APP_ROOT"/python
 
 PYTHON_PACKAGE="http://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.bz2"
-SETUPTOOLS_PACKAGE="http://pypi.python.org/packages/source/s/setuptools/setuptools-${SETUPTOOLS_VERSION}.tar.gz"
 
 # Install Python
 mkdir -p "$PACKAGES_ROOT"
@@ -27,12 +25,6 @@ cd Python-"${PYTHON_VERSION}"/
 ./configure --prefix="$PYTHON_ROOT"
 make
 make install
-
-# Install setuptools that provides easy_install
-cd "$PACKAGES_ROOT"
-curl "$SETUPTOOLS_PACKAGE" | tar xvz
-cd setuptools-"$SETUPTOOLS_VERSION"
-"$PYTHON_ROOT"/bin/python setup.py install
 
 # Clean up installation packages
 cd "$APP_ROOT"

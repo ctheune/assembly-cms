121bb7d36fcff795f7a0a9ca82f3ce5595a4ee58
Christian Theune <ct@gocept.com>
2009-12-10 22:47:59 +0100

    Add preliminary shot at banners.

---
+++ extensions/asm.banner/src/asm/banner/banner.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2009 Assembly Organizing
+# See also LICENSE.txt
+
+import asm.cms.interfaces
+import datetime
+import grok
+import zope.component
+import zope.event
+import pytz
+
+
+class BannerManager(grok.Container):
+    pass

9f12783418bb6bacd8a6a4cf03cbdfa9a5ab8a3d
Christian Theune <ct@flyingcircus.io>
2009-12-10 10:30:05 +0100

    - Remove links to wwwadmin - Implement news portlet - Implement image field for news - Use image and teaser fields for news on frontpage - Implement generic copyFrom-support for extension schemas - Only supply the news extension schema for html pages within a news section

--- src/asm/cms/news.py
+++ src/asm/cms/news.py
@@ -30,19 +30,61 @@ class NewsFolder(asm.cms.Edition):
                     yield page
 
 
-class ITeaser(zope.interface.Interface):
+class INewsFields(zope.interface.Interface):
 
     teaser = zope.schema.TextLine(title=u'Teaser text')
+    image = zope.schema.Bytes(title=u'File')
 
 
-class TeaserAnnotation(grok.Annotation):
-    grok.implements(ITeaser)
+class TeaserAnnotation(grok.Annotation,
+                       grok.Model):
+    grok.implements(INewsFields)
+    grok.provides(INewsFields)
     grok.context(asm.cms.interfaces.IEdition)
 
     teaser = u''
 
+    def copyFrom(self, other):
+        self.teaser = other.teaser
+        self.image = other.image
+
+    def set_image(self, value):
+        edition = self.__parent__
+        if not 'teaser-image' in edition.page:
+            image = asm.cms.page.Page()
+            image.type = 'asset'
+            edition.page['teaser-image'] = image
+        image = edition.page['teaser-image']
+        image_edition = image.getEdition(edition.parameters, create=True)
+        image_edition.content = value
+
+    def get_image(self):
+        edition = self.__parent__
+        if not 'teaser-image' in edition.page:
+            return None
+        image = edition.page['teaser-image']
+        image_edition = image.getEdition(self.__parent__.parameters,
+                                         create=True)
+        return image_edition.content
+
+    image = property(fget=get_image, fset=set_image)
+
+
+@grok.subscribe(asm.cms.htmlpage.HTMLPage)
+@grok.implementer(asm.cms.interfaces.IAdditionalSchema)
+def add_teaser(edition):
+    page = edition.page
+    while page:
+        if not asm.cms.interfaces.IPage.providedBy(page):
+            break
+        if page.type == 'news':
+            return INewsFields
+        page = page.__parent__
+
 
 class Edit(asm.cms.EditForm):
+
+    grok.context(NewsFolder)
 
     form_fields = grok.AutoFields(asm.cms.interfaces.IEdition).select(
         'title', 'tags', 'modified')
@@ -50,13 +92,9 @@ class Edit(asm.cms.EditForm):
     form_fields['modified'].location = 'side'
 
 
-@grok.subscribe(asm.cms.htmlpage.HTMLPage)
-@grok.implementer(asm.cms.interfaces.IAdditionalSchema)
-def add_teaser(page):
-    return ITeaser
-
-
 class Index(asm.cms.Pagelet):
+
+    grok.context(NewsFolder)
 
     def list(self):
         for news in self.context.list():

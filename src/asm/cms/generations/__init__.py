9a1be108f0ff8ca8fcd78cad55e69684258fcc96
Christian Theune <ct@gocept.com>
2010-01-18 17:20:23 +0200

    Provide fix for naive datetimes still around.

--- src/asm/cms/generations/__init__.py
+++ src/asm/cms/generations/__init__.py
@@ -2,6 +2,6 @@ import zope.app.generations.generations
 
 
 manager = zope.app.generations.generations.SchemaManager(
-    minimum_generation=0,
-    generation=0,
-    package_name='asm.cms')
+    minimum_generation=2,
+    generation=2,
+    package_name='asm.cms.generations')

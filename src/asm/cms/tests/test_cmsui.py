8d0b5e5c3ceffdad1b6bb9c25b701e71cac3fa16
Christian Theune <ct@gocept.com>
2010-01-24 12:05:14 +0200

    Fix breadcrumb link target.

--- src/asm/cms/tests/test_cmsui.py
+++ src/asm/cms/tests/test_cmsui.py
@@ -1,22 +1,23 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
+import asm.cms.page
 import asm.cms.testing
+import transaction
 import unittest
 
 
 class CMSUI(asm.cms.testing.SeleniumTestCase):
 
     def test_cms_redirects_to_editor(self):
-        s = self.selenium
-        s.open('http://mgr:mgrpw@%s/++skin++cms/cms' % s.server)
+        self.selenium.open('http://mgr:mgrpw@%s/++skin++cms/cms' %
+                           self.selenium.server)
         self.assertEquals(
             u'http://localhost:8087/++skin++cms/cms/edition-/@@edit',
-            s.getLocation())
+            self.selenium.getLocation())
 
     def test_switch_to_navigation_and_back(self):
         s = self.selenium
-        s.open('http://mgr:mgrpw@%s/++skin++cms/cms' % s.server)
         s.assertNotVisible("css=#navigation")
         s.assertVisible("css=#content")
 
@@ -27,3 +28,21 @@ class CMSUI(asm.cms.testing.SeleniumTestCase):
         s.click('css=#navigation-actions .toggle-navigation')
         s.assertNotVisible("css=#navigation")
         s.assertVisible("css=#content")
+
+    def test_breadcrumbs(self):
+        # We need to add a sub-page as the root never shows up in the
+        # breadcrumbs
+        self.cms['xy'] = asm.cms.page.Page('htmlpage')
+        self.cms['xy'].editions.next().title = u'A test page'
+        transaction.commit()
+        s = self.selenium
+        s.open(
+            'http://mgr:mgrpw@%s/++skin++cms/cms/xy/edition-/@@edit' %
+            s.server)
+        s.assertVisible(
+            'xpath=//div[contains(@class, "breadcrumbs")]/'
+            'a[contains(text(), "A test page")]')
+        s.clickAndWait(
+            'xpath=//div[contains(@class, "breadcrumbs")]/'
+            'a[contains(text(), "A test page")]')
+        s.assertElementPresent('name=form.actions.save')

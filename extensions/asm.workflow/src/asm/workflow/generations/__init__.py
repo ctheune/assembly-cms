d06908ea035067ca3ea76a53886e8eeac0f17cae
Christian Theune <ct@gocept.com>
2010-04-12 16:28:21 +0200

    Extract asm.translation and asm.workflow into individual packages.

--- extensions/asm.workflow/src/asm/workflow/generations/__init__.py
+++
@@ -1,7 +0,0 @@
-import zope.app.generations.generations
-
-
-manager = zope.app.generations.generations.SchemaManager(
-    minimum_generation=1,
-    generation=1,
-    package_name='asm.workflow.generations')
--- extensions/asm.workflow/src/asm/workflow/interfaces.py
+++
@@ -1,7 +0,0 @@
-
-
-class PublishedEvent(object):
-
-    def __init__(self, draft, public):
-        self.draft = draft
-        self.public = public

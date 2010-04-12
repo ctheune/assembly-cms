f913857eb4df498ff60aa384a2a48a3de7358f38
Christian Theune <ct@gocept.com>
2010-04-12 16:41:03 +0200

    Back to development: 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cms',
-      version = '0.1.2',
+      version = '0.1.3dev',
       description="Assembly Website CMS",
       author="Assembly Webcrew",
       author_email="web@assembly.org",

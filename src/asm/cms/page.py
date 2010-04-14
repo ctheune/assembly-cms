10ecf6a1601f8b061be0a5668306efd984a2f398
Christian Theune <ct@flyingcircus.io>
2010-04-14 10:50:06 +0200

    Retrieve the skin that a site uses from the profile.

--- src/asm/cms/page.py
+++ src/asm/cms/page.py
@@ -217,8 +217,9 @@ class Preview(grok.View):
     grok.context(asm.cms.interfaces.IPage)
 
     def render(self):
+        skin_name = zope.component.getUtility(asm.cms.interfaces.ISkinProfile)
         skin = zope.component.getUtility(
-            zope.publisher.interfaces.browser.IBrowserSkinType, 'summer09')
+            zope.publisher.interfaces.browser.IBrowserSkinType, skin_name)
         edition = asm.cms.edition.select_edition(self.context, self.request)
         zope.publisher.browser.applySkin(self.request, skin)
         return zope.component.getMultiAdapter(

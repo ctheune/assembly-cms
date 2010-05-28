cd2f0db5368570649e3f0bd3a0a9698f8b54ea56
Christian Theune <ct@gocept.com>
2010-05-28 14:49:19 +0300

    Turn assets to using blobs.
    Re #6328

--- src/asm/cms/generations/__init__.py
+++ src/asm/cms/generations/__init__.py
@@ -2,6 +2,6 @@ import zope.app.generations.generations
 
 
 manager = zope.app.generations.generations.SchemaManager(
-    minimum_generation=3,
-    generation=3,
+    minimum_generation=4,
+    generation=4,
     package_name='asm.cms.generations')

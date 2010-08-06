edd6adfdbd1f8430237266752e5c5d6408f1b928
Christian Theune <ct@gocept.com>
2010-08-06 22:23:28 +0200

    Implement more media gallery stuff including public skinning, annotation data, ...

---
+++ extensions/asm.mediagallery/src/asm/mediagallery/externalasset.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import asm.cms.edition
+import asm.mediagallery.interfaces
+import grok
+import persistent
+import zope.interface
+import zope.app.form.browser.objectwidget
+
+
+class ExternalAsset(asm.cms.edition.Edition):
+
+    zope.interface.implements(asm.mediagallery.interfaces.IExternalAsset)
+    zope.interface.classProvides(asm.cms.interfaces.IEditionFactory)
+
+    factory_title = u'External asset'
+
+    thumbnail = None
+    locations = ()
+
+    def copyFrom(self, other):
+        super(ExternalAsset, self).copyFrom(other)
+        # XXX deepcopy
+        self.thumbnail = other.thumbnail
+        self.locations = other.locations
+
+    def __eq__(self, other):
+        if not super(ExternalAsset, self).__eq__(other):
+            return False
+        return (self.thumbnail == other.thumbnail and
+                self.locations == other.locations)
+
+
+def setupObjectInputWidget(field, request):
+    factory = zope.component.getUtility(zope.component.interfaces.IFactory,
+                                        name=field.schema.__name__)
+    return zope.app.form.browser.objectwidget.ObjectWidget(field, request, factory)
+
+
+class Edit(asm.cms.form.EditionEditForm):
+
+    grok.context(ExternalAsset)
+
+    main_fields = grok.AutoFields(ExternalAsset).select(
+        'title', 'thumbnail', 'locations')
+    main_fields['thumbnail'].custom_widget = (
+        asm.cms.asset.FileWithDisplayWidget)
+
+
+class Index(asm.cms.Pagelet):
+
+    grok.context(ExternalAsset)
+
+    def embed(self):
+        for service_choice in self.context.locations:
+            try:
+                service = zope.component.getUtility(
+                    asm.mediagallery.interfaces.IEmbeddableContentHostingService,
+                    name=service_choice.service_id)
+            except LookupError:
+                pass
+            else:
+                return service.embed_code(service_choice.id)
+
+    def links(self, include=None, exclude=None, max=None):
+        count = 0
+        for service_choice in self.context.locations:
+            if include and service_choice.service_id not in include:
+                continue
+            if exclude and service_choice.service_id in exclude:
+                continue
+            service = zope.component.getUtility(
+                asm.mediagallery.interfaces.IContentHostingService,
+                name=service_choice.service_id)
+            yield service.link_code(service_choice.id)
+            count += 1
+            if count == max:
+                return
+
+
+class Thumbnail(grok.View):
+
+    def render(self):
+        return open(self.context.thumbnail.committed())
+
+
+class HostingServiceChoice(persistent.Persistent):
+
+    zope.interface.implements(
+        asm.mediagallery.interfaces.IHostingServiceChoice)

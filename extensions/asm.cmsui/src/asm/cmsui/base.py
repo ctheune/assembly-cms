7917dcb550c27a0407dd3c365612158fb080bf70
Christian Theune <ct@gocept.com>
2010-08-08 19:35:48 +0200

    Move more stuff around to get a reasonable module API in CMSUI and fix the core tests again.

---
+++ extensions/asm.cmsui/src/asm/cmsui/base.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import grok
+import asm.cmsui.interfaces
+
+
+class ActionView(grok.View):
+
+    grok.baseclass()
+    grok.layer(asm.cms.interfaces.ICMSSkin)
+
+    def render(self):
+        self.redirect(self.url(self.context, '@@edit'))
+
+
+class PageHeader(grok.ViewletManager):
+    grok.context(zope.interface.Interface)
+    grok.name('pageheader')
+
+
+class MainPageActions(grok.ViewletManager):
+
+    grok.name('main-page-actions')
+    grok.context(zope.interface.Interface)
+
+
+class ExtendedPageActions(grok.ViewletManager):
+
+    grok.name('extended-page-actions')
+    grok.context(zope.interface.Interface)
+
+
+class PageActionGroups(grok.ViewletManager):
+
+    grok.name('page-action-groups')
+    grok.context(zope.interface.Interface)
+
+
+class NavigationActions(grok.ViewletManager):
+
+    grok.name('navigation-actions')
+    grok.context(zope.interface.Interface)
+
+
+class NavigationToolActions(grok.ViewletManager):
+
+    grok.name('navigation-tool-actions')
+    grok.context(zope.interface.Interface)
+
+
+class NotificationMessages(grok.ViewletManager):
+
+    grok.context(zope.interface.Interface)
+    grok.name('notification-messages')
---
+++ extensions/asm.cmsui/src/asm/cmsui/interfaces.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+
+class ICMSSkin(grok.IDefaultBrowserLayer):
+    grok.skin('cms')
+
+
+class IRetailSkin(grok.IDefaultBrowserLayer):
+    grok.skin('retail')

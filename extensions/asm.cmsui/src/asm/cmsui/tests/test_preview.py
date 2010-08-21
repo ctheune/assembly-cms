3dd91771d30dcb4905db078cc1d112dfbda3c24e
Christian Theune <ct@gocept.com>
2010-08-21 16:50:41 +0200

    get cmsui tests working again

--- extensions/asm.cmsui/src/asm/cmsui/tests/test_preview.py
+++ extensions/asm.cmsui/src/asm/cmsui/tests/test_preview.py
@@ -1,14 +1,14 @@
 # Copyright (c) 2010 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import asm.cms.testing
+import asm.cmsui.testing
 import zope.testbrowser.testing
 
 
-class PreviewTests(asm.cms.testing.FunctionalTestCase):
+class PreviewTests(asm.cmsui.testing.SeleniumTestCase):
 
     def test_preview_not_broken(self):
         # Very simple smoke test to ensure that the preview template actually
         # renders. I've seen this break when renaming static resources.
-        b = zope.testbrowser.testing.Browser()
-        b.open('http://localhost/cms/@@preview-window')
+        self.selenium.open('http://mgr:mgrpw@%s/++skin++cms/cms/@@preview-window' %
+                           self.selenium.server)

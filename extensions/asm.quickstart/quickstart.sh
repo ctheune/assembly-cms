8b9185edf65181748946731a082ed234f570b3e8
Christian Theune <ct@gocept.com>
2010-08-22 18:16:27 +0200

    Small improvement in quickstart code.

--- extensions/asm.quickstart/quickstart.sh
+++ extensions/asm.quickstart/quickstart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
+
 echo "Assembly CMS quickstart installer"
 echo
-echo "Fetching and building Assembly CMS ..."
-
+echo "Installing Assembly CMS. This can take a few minutes."
+echo
+echo "Fetching code ..."
 hg -q clone http://bitbucket.org/ctheune/assembly-cms
 cd assembly-cms
 cat > buildout.cfg <<EOF
@@ -13,11 +16,20 @@ extends = profiles/base.cfg
 admin-password = admin
 EOF
 
-python2.5 bootstrap.py
-bin/buildout -q
+echo "Bootstrapping ..."
+python2.5 bootstrap.py > /dev/null
+
+echo "Building ..."
+bin/buildout -q > /dev/null 
 
 ./etc/init.d/asm.cms-zeo start > /dev/null
 ./etc/init.d/asm.cms-server start > /dev/null
 
+echo "Done."
+echo
 echo "Server is running at http://localhost:8081."
 echo "The initial username is 'admin' and the password is 'admin'."
+echo 
+echo "You can stop the server and database by executing these commands:"
+echo "./etc/init.d/asm.cms-server stop"
+echo "./etc/init.d/asm.cms-zeo stop"

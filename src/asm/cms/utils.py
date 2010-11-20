199189af772d59b55e82160a1d3c157b05daf0aa
Tapanila <Tapanila@localhost>
2010-11-20 16:34:18 +0100

    Paragraph checking

--- src/asm/cms/utils.py
+++ src/asm/cms/utils.py
@@ -41,3 +41,33 @@ def normalize_name(title):
     result = re.sub("-+", "-", result)
     result = result.strip("-")
     return result
+
+def remove_empty_paragraph(element):
+    text = element.text
+    if text is None:
+        element.getparent().remove(element)
+    if text == '':
+        element.getparent().remove(element)
+
+def normalize_whitespaces(text):
+    if text is None:
+        return text
+    return text.strip()
+
+def check_paragraphs(html):
+    parser = lxml.etree.HTMLParser()
+    document = (
+        '<stupidcontainer>%s</stupidcontainer>' % html)
+    document = lxml.etree.fromstring(document, parser)
+    htmltags =  ['//pre','//head','//javascript']
+    for locator in htmltags:
+        for element in document.xpath(locator):
+            element.text = normalize_whitespaces(element.text)
+            remove_empty_paragraph(element)
+
+    result = lxml.etree.tostring(document.xpath('//stupidcontainer')[0],
+        pretty_print=True)
+    result = result.replace('<stupidcontainer>', '')
+    result = result.replace('</stupidcontainer>', '')
+    result = result.replace('<stupidcontainer/>', '') #This is the case when there's nothing to return
+    return result.strip()

b2f39960fdab892de4df2b0612f3a837ff4b6ee0
Christian Theune <ct@gocept.com>
2011-01-07 17:59:27 +0100

    Preparing release 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cms',
-      version = '0.1.5dev',
+      version='0.1.5',
       description="Assembly Website CMS",
       author="Assembly Webcrew",
       author_email="web@assembly.org",

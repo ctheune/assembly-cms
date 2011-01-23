58c1d07c4b0bcfc92bc4103576f14b0768b22fff
Christian Theune <ct@flyingcircus.io>
2011-01-23 20:32:40 +0100

    Preparing release 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='asm.cms',
-      version='0.1.8dev',
+      version='0.1.8',
       description="Assembly Website CMS",
       author="Assembly Webcrew",
       author_email="web@assembly.org",

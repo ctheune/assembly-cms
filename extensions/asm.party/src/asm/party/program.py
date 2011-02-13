5dd2291f69076c3e6fde95d79e0b167c3ffc3825
Christian Theune <ct@gocept.com>
2011-02-13 12:34:38 +0200

    * Activate asm.archive profile generally * Move asm.party profile only to summer11 skin * Split off a basic (non-competition) event type

--- extensions/asm.party/src/asm/party/program.py
+++ extensions/asm.party/src/asm/party/program.py
@@ -25,6 +25,34 @@ class EditSection(asm.cmsui.form.EditionEditForm):
     grok.name('edit')
 
 
+class Event(asm.cms.edition.Edition):
+
+    zope.interface.implements(asm.party.interfaces.IEvent)
+    zope.interface.classProvides(asm.cms.interfaces.IEditionFactory)
+
+    factory_title = u'Event'
+    factory_visible = staticmethod(
+        lambda parent:parent.factory is ProgramSection)
+
+    headline = u''
+    description = u''
+    location = u''
+    time = None
+
+    def copyFrom(self, other):
+        super(Event, self).copyFrom(other)
+        self.headline = other.headline
+        self.description = other.description
+        self.location = other.location
+        self.time = other.time
+
+
+class EditEvent(asm.cmsui.form.EditionEditForm):
+
+    grok.context(Event)
+    main_fields = grok.AutoFields(asm.party.interfaces.IEvent)
+
+
 class Competition(asm.cms.edition.Edition):
 
     zope.interface.implements(asm.party.interfaces.ICompetition)
@@ -34,20 +62,12 @@ class Competition(asm.cms.edition.Edition):
     factory_visible = staticmethod(
         lambda parent:parent.factory is ProgramSection)
 
-    headline = u''
-    description = u''
-    location = u''
-    time = None
     prizes = u''
     participation_info = None
     gallery = None
 
     def copyFrom(self, other):
         super(Competition, self).copyFrom(other)
-        self.headline = other.headline
-        self.description = other.description
-        self.location = other.location
-        self.time = other.time
         self.prizes = other.prizes
         self.participation_info = other.participation_info
         self.gallery = other.gallery
@@ -57,12 +77,3 @@ class EditCompetition(asm.cmsui.form.EditionEditForm):
 
     grok.context(Competition)
     main_fields = grok.AutoFields(asm.party.interfaces.ICompetition)
-
-
-class MusicEvent(asm.cms.edition.Edition):
-
-    zope.interface.classProvides(asm.cms.interfaces.IEditionFactory)
-
-    factory_title = u'Music event'
-    factory_visible = staticmethod(
-        lambda parent:parent.factory is ProgramSection)

19aa01ae6182ed430dfbefb9c997bd82ada83f9e
Christian Theune <ct@flyingcircus.io>
2011-05-14 16:16:14 +0300

    optimize section headers away if no editions are there

--- extensions/asm.org/src/asm/org/skin.py
+++ extensions/asm.org/src/asm/org/skin.py
@@ -40,64 +40,16 @@ class LayoutHelper(grok.View):
             candidate = candidate.__parent__
         else:
             return
+        editions = list(asm.cms.edition.find_editions(
+            candidate, request=self.request, recurse=False))
+        if not editions:
+            return
         return dict(section=asm.cms.edition.select_edition(
                         candidate, self.request),
-                    subs=asm.cms.edition.find_editions(
-                        candidate, request=self.request, recurse=False))
+                    subs=editions)
 
     # A helper class to get access to the static directory in this module from
     # the layout.
 
     def render(self):
         return ''
-
-
-class Navtree(grok.View):
-    grok.layer(ISkin)
-    grok.context(zope.interface.Interface)
-
-    def update(self):
-        self.active = []
-        current = self.context.page
-        while current:
-            self.active.append(current)
-            current = current.__parent__
-
-    def _create_subtree(self, root, levels):
-        if levels < 0:
-            return
-        if root.type in ['asset']:
-            return
-        edition = asm.cms.edition.select_edition(root, self.request)
-        if edition.has_tag('hide-navigation'):
-            return
-        if isinstance(edition, asm.cms.edition.NullEdition):
-            return
-        tree = {'page': edition,
-                'class': set(),
-                'subpages': []}
-        if root in self.active:
-            tree['class'].add('active')
-            for child in root.subpages:
-                sub_tree = self._create_subtree(child, levels - 1)
-                if sub_tree:
-                    tree['subpages'].append(sub_tree)
-        if 'active' in tree['class'] and not tree['subpages']:
-            tree['class'].add('has_no_children')
-        tree['class'] = ' '.join(tree['class'])
-        return tree
-
-    def tree(self):
-        root = self.application
-
-        tree = self._create_subtree(root, 3)
-        return tree['subpages']
-
-    @property
-    def page(self):
-        if asm.cms.interfaces.IEdition.providedBy(self.context):
-            return self.context.__parent__
-        return self.context
-
-    def css_classes(self, *classes):
-        return ' '.join(filter(None, classes))

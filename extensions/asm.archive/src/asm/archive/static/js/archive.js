07c367d018235e5032bf70d217dc9eff76e4dfc3
Jussi Judin <jjudin+asmcms@iki.fi>
2011-08-12 02:35:53 +0300

    Remove jquery from the loading script.

--- extensions/asm.archive/src/asm/archive/static/js/archive.js
+++ extensions/asm.archive/src/asm/archive/static/js/archive.js
@@ -1,8 +1,8 @@
-$(function() {
+(function() {
     // This should show placeholders in IE.
     $(':input[placeholder]').placeholder();
     // Load Facebook and Twitter buttons.
     $('.link-button').each(function() {
         $(this).attr('src', $(this).data('src'));
     });
-});
+})();

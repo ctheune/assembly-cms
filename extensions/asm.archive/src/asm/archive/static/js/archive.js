c29ac43b11ba20b56e282275e99684a4e4aa45e9
Jussi Judin <462528+Barro@users.noreply.github.com>
2012-01-30 01:50:59 +0200

    Make navigation elements larger on archive mobile view.

--- extensions/asm.archive/src/asm/archive/static/js/archive.js
+++ extensions/asm.archive/src/asm/archive/static/js/archive.js
@@ -1,13 +1,20 @@
 $(function() {
     // This should show placeholders in IE.
     $(':input[placeholder]').placeholder();
-    // Load Facebook and Twitter buttons.
-    $('.link-button').each(function() {
-        $(this).attr('src', $(this).data('src'));
-    });
+    var screen_width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-    // Google +1 button.
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/plusone.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    // Load like buttons only on the desktop version:
+    // Loading these buttons causes really long loading indicator display
+    // and hides all navigation buttons (at least on Android 2.3 browser with
+    // slow network connection).
+    if (screen_width > 680) {
+        // Load Facebook and Twitter buttons.
+        $('.link-button').each(function() {
+            $(this).attr('src', $(this).data('src'));
+        });
+        // Google +1 button.
+        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    }
 });

89705ce9cc30b5d5293c95e0e644afcbd89f163b
Christian Theune <ct@flyingcircus.io>
2012-05-12 21:44:36 +0300

    Revamp the summer 12 skin and the layoutpage mechanism to provide a flexible layout selection independent of inserting fake pages.
    Layout pages are now content objects that don't get directly published and can
    be choosen as a layout from each other content object.

---
+++ extensions/asm.layoutpage/src/asm/layoutpage/layouts.py
@@ -0,0 +1,10 @@
+import asm.layoutpage.interfaces
+import asm.layoutpage.layoutpage
+import grok
+
+
+
+class DefaultLayout(asm.layoutpage.layoutpage.Layout, grok.GlobalUtility):
+    grok.provides(asm.layoutpage.interfaces.ILayoutPage)
+
+    layout = '${}'

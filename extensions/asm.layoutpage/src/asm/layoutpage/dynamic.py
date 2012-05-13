7d114ad23a2cd82cfe2a2dfc9b5219d5822a4e8e
Jussi Judin <jjudin+asmcms@iki.fi>
2012-05-13 17:42:03 +0300

    Copy subtree navigation code to create section navigation.

--- extensions/asm.layoutpage/src/asm/layoutpage/dynamic.py
+++ extensions/asm.layoutpage/src/asm/layoutpage/dynamic.py
@@ -23,6 +23,7 @@ class TileNavigation(grok.View):
 
         self.rows.append(row)
 
+NAVTREE_MAX_LEVELS = 999
 
 class SectionNavigation(grok.View):
     grok.context(asm.cms.interfaces.IEdition)
@@ -43,6 +44,42 @@ class SectionNavigation(grok.View):
             if edition.has_tag('hide-navigation'):
                 continue
             self.subsections.append(edition)
+
+        self.active = []
+        current = self.context.page
+        while current:
+            self.active.append(current)
+            current = current.__parent__
+
+    def _create_subtree(self, root, levels):
+        if levels < 0:
+            return
+        if root.type in ['asset']:
+            return
+        edition = asm.cms.edition.select_edition(root, self.request)
+        if edition.has_tag('hide-navigation'):
+            return
+        if isinstance(edition, asm.cms.edition.NullEdition):
+            return
+        tree = {'page': edition,
+                'class': set(),
+                'subpages': []}
+        if root in self.active:
+            tree['class'].add('active')
+            if root.type not in ['news']:
+                for child in root.subpages:
+                    sub_tree = self._create_subtree(child, levels - 1)
+                    if sub_tree:
+                        tree['subpages'].append(sub_tree)
+        if 'active' in tree['class'] and not tree['subpages']:
+            tree['class'].add('has_no_children')
+        tree['class'] = ' '.join(tree['class'])
+        return tree
+
+    def tree(self):
+        root = self.find_section()
+        tree = self._create_subtree(root, NAVTREE_MAX_LEVELS)
+        return tree['subpages']
 
 
 class BaseUrl(grok.View):

58d5baf54f42a6a4014c892bd21b55e79736191a
Christian Theune <ct@gocept.com>
2012-05-14 21:20:36 +0200

    Mini-version of summer skin. Again.

---
+++ extensions/asm.summer12mini/src/asm/summer12mini/i18n.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zope.i18nmessageid
+import zope.i18n
+
+_ = MessageFactory = zope.i18nmessageid.MessageFactory('asm.summer12mini')

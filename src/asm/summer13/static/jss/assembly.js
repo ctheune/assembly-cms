68f59245d4eda9e3e8dcd5afba5912383ab51f95
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-08-02 15:56:17 +0300

    Added AssemblyTV schedule under stream player.

--- src/asm/summer13/static/jss/assembly.js
+++ src/asm/summer13/static/jss/assembly.js
@@ -22,20 +22,18 @@ function initNewsRotation() {
 }
 
 function initStreamOverlay() {
-   // If the function argument is given to overlay,
-   // it is assumed to be the onBeforeLoad event listener.
-   $('a[rel="#overlay"]').overlay({
-      mask: 'black',
-      effect: 'apple',
-      onBeforeLoad: function wrapOverlay() {
-
-         // grab wrapper element inside content
-         var wrap = this.getOverlay().find(".wrap");
-
-         // load the page specified in the trigger
-         wrap.load(this.getTrigger().attr("href"));
-      }
-   });
+    // If the function argument is given to overlay,
+    // it is assumed to be the onBeforeLoad event listener.
+    $('a[rel="#overlay"]').overlay({
+        mask: 'black',
+        effect: 'apple',
+        onBeforeLoad: function wrapOverlay() {
+            // Grab wrapper element inside content.
+            var wrap = this.getOverlay().find(".wrap");
+            // Load the page specified in the trigger.
+            wrap.load(this.getTrigger().attr("href"), initAssemblyTVSchedule);
+        }
+    });
 }
 
 function initCountDown() {
@@ -51,7 +49,7 @@ function initTwitter() {
 
     // Load latest tweets ticker.
     $.getScript("//widgets.twimg.com/j/2/widget.js", function tweetScriptLoaded() {
-      if ($('#twitter-latest-tweets').length > 0)
+        if ($('#twitter-latest-tweets').length > 0)
         new TWTR.Widget({
             id: "twitter-latest-tweets",
             version: 2,
@@ -81,7 +79,7 @@ function initTwitter() {
             }
         }).render().start();
 
-      if ($('#twitter-latest-tweets-asmtv').length > 0)
+    if ($('#twitter-latest-tweets-asmtv').length > 0)
         new TWTR.Widget({
             id: "twitter-latest-tweets-asmtv",
             version: 2,
@@ -110,5 +108,52 @@ function initTwitter() {
                 behavior: 'default'
             }
         }).render().start();
+    });
+}
+
+function initAssemblyTVSchedule() {
+    function pad(n, width, z) {
+        z = z || '0';
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
+
+    var comingUp;
+    // Create container element.
+    if (!$('.coming-up').length) {
+        comingUp = $('<ul>');
+        $('<div>').attr('class', 'coming-up').appendTo('.overlay > .wrap')
+            .append($('<h3>AssemblyTV</h3>: <span class="now"></span>'))
+            .append(comingUp);
+    } else {
+        comingUp = $('.coming-up ul').empty();
+    }
+
+    $.getJSON('activities/assemblytv/schedule/json', function (data, status) {
+        if (data && data.events && data.events.length) {
+            // Filter past events and grab 3 next events.
+            var updateDelay, events = data.events.filter(function eventFilter(item, i) {
+                return (new Date(item.time)).getTime() > Date.now() ||
+                    (new Date(item.end_time)).getTime() > Date.now();
+            }).splice(0, 3);
+            console.log(events);
+            events.forEach(function eventIterator(event, i) {
+                var d = new Date(event.time),
+                    diff = d.getTime() - Date.now(); //, minutes, hours, days;
+                // Update the list 5 seconds after the first event in the list starts.
+                if (i === 1) { updateDelay = diff + 5000; }
+
+                var time = pad(d.getHours(), 2) + ':' + pad(d.getMinutes(), 2);
+                // Append to HTML.
+                if (!i) {
+                    $('.coming-up .now').text(event.name);
+                } else {
+                    $('<li>').text((!i ? 'now' : time) + ' – ' + event.name)
+                        .appendTo(comingUp);
+                }
+            });
+            $('<li></li>').html('&hellip;').appendTo(comingUp);
+            setTimeout(initAssemblyTVSchedule, updateDelay);
+        }
     });
 }
